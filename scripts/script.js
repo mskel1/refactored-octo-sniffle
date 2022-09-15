f2e8861224ce469e0f13e8bbfcb108ee22260f2f
mskel1 <111534244+mskel1@users.noreply.github.com>
2022-09-15 14:45:29 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,7 @@
 $(document).ready(function() {
   //this is an immediately invoked function expression (iife)
   //click on an image, get a message
-  
-  $("#dogeOne").click(function (){
-    
-    $("#outputDiv").hide();
-    
-    console.log("user clicked dogeOne");
-    $("#outputDiv").toggle();
-    $("#outputMsg").text("You Lose, you Stupid Loser");
-  });
-  
-  $("#dogeTwo").click(function (){
-    
-    $("#outputDiv").hide();
-    
-    console.log("user clicked dogeTwo");
-    $("#outputDiv").toggle();
-    $("#outputMsg").text("you has WOW");
-  });
+ 
 });
 
 function nameLength(name) {
